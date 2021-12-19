e8b59a09ace9b8d4145b5d0e2afd1aac456412fb
aleksey201704 <89303424+aleksey201704@users.noreply.github.com>
2021-12-19 09:54:38 +0500

    19.12.2021

--- Fraction/Source.cpp
+++ Fraction/Source.cpp
@@ -4,15 +4,70 @@ using namespace std;
 
 class Fraction
 {
+	
 public:
 	int Numerator;
 	int Denominator;
 
-	
+	// -----------Constructors----------------
+	Fraction()
+	{
+
+	}
+
+	Fraction(int Numerator, int Denominator)
+	{
+		this->Numerator=Numerator;
+		this->Denominator=Denominator;
+		cout << "Адрес конструтора = " << this << endl;
+		cout << "\n------------------------------------\n";
+	}
+
+	// Сopying the constructor
+
+	/*Fraction(const Fraction& other) 
+	{
+		this->Numerator = other.Numerator;
+		this->Denominator = other.Denominator;
+		cout << "Адрес Копии конструтора = " << this << endl;
+		cout << "\n------------------------------------\n";
+	}*/
+
+	~Fraction()
+	{
+		
+		cout << "Адрес Десконструтора = " << this << endl;
+		cout << "\n------------------------------------\n";
+	}
+
+	// Methods
+
+	void Print()const
+	{
+		cout<< Numerator << "/" << Denominator<<endl ;
+	}
 };
+
+Fraction operator+(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	result.Numerator = left.Numerator + right.Numerator;
+	result.Denominator = right.Denominator + left.Denominator;
+	return result;
+}
 
 void main() 
 {
 	setlocale(LC_ALL, "");
+
+	Fraction A(10,14) ;
+	Fraction B(10,25);
+	Fraction C = A + B;
+	//A = B;
+	A.Print();
+	B.Print();
+	C.Print();
+
+	
 
 }
